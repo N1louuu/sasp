738791610bbb51c499d994fe92bafc7c07316203
nilo <135813057+N1louuu@users.noreply.github.com>
2025-02-03 12:10:13 +0200

    CRUD

--- dbfunctions.php
+++ dbfunctions.php
@@ -3,7 +3,7 @@
 function connect() {
     $servername = "nilsch24.treok.io";
     $username = "nilsch24_sasp";
-    $password = `Y\$H8~uL.P^j*`;
+    $password = "v}PrFPu?OLU8";
     //$port = 3306;
     $dbname = "nilsch24_db-sasp";
 
@@ -11,12 +11,53 @@ function connect() {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+        //echo "Connected successfully";
         return $conn;
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
         die();
     }
 }
+
+function getAllGames() {
+    $pdo = connect();
+    $sql = "SELECT * FROM test_games";
+    $stm = $pdo->query($sql);
+    $games = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $games;
+}
+
+function getGameById($id) {
+    $pdo = connect();
+    $sql = "SELECT * FROM test_games WHERE gameid=?";
+    $stm = $pdo->prepare($sql);
+    $stm->execute([$id]);
+    $game = $stm->fetch(PDO::FETCH_ASSOC);
+    return $game;
+}  
+
+function insertNewGame($name, $company, $release) {
+    $pdo = connect();
+    $sql = "INSERT INTO test_games (`name`, company, `release`) VALUES (?, ?, ?)";
+    $stm = $pdo->prepare($sql);
+    $ok = $stm->execute([$name, $company, $release]);
+    return $ok;
+}
+
+function updateGame($name, $company, $release, $id) {
+    $pdo = connect();
+    $sql = "UPDATE test_games SET `name`=?, company=?, `release`=? WHERE gameid=?";
+    $stm = $pdo->prepare($sql);
+    $ok = $stm->execute([$name, $company, $release, $id]);
+    return $ok;
+}
+
+function deleteGame($id) {
+    $pdo = connect();
+    $sql = "DELETE FROM test_games WHERE gameid=?";
+    $stm = $pdo->prepare($sql);
+    $ok = $stm->execute([$id]);
+    return $ok;
+}  
 
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sasp</title>
-</head>
-<body>
-    
 <?php
 
 require "dbfunctions.php";
 
 $pdo = connect();
 
+$games = getAllGames();
+
+function printAllGames( $games ) {
+    foreach ($games as $game) {
+        echo "<div class='card d-flex flex-row justify-content-between'>";
+        foreach ($game as $thing) {
+            echo "<p>" . $thing . " </p>";
+        }
+        echo "<form action='' method='post'>";
+        echo "<input type='hidden' name='id' value='" . $game["gameid"] . "'>";
+        echo "<input type='submit' value='poista' name='delete' class='btn btn-danger'>";
+        echo "</form>";
+        echo "</div>";
+        echo "<br>";
+    }
+    
+}
+
+if (isset($_POST["delete"])) {
+    $id = $_POST["id"];
+    deleteGame($id);
+}
+
+if (isset($_POST["name"]) && isset($_POST["company"]) && isset($_POST["release"])) {
+    $name = $_POST["name"];
+    $company = $_POST["company"];
+    $release = $_POST["release"];
+    insertNewGame($name, $company, $release);
+}
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> 
+    <title>sasp</title>
+</head>
+<body>
+    <div class="d-flex flex-row">
+        <div class="w-50 m-2">
+            <h1>All Games</h1>
+            <?php
+                printAllGames($games);
+            ?>
+        </div>
+        <form class="w-50 m-5 d-flex flex-column" action="" method="post">
+            <h1>Create a new game</h1>
+            
+            <label for="name">nimi: </label>
+            <input type="text" name="name" class="form-control">
+            
+            <label for="company">company: </label>
+            <input type="text" name="company" class="form-control">
+            
+            <label for="release">release: </label>
+            <input type="number" name="release" class="form-control">
+            
+            <input type="submit" class="btn btn-primary mt-2" value="uusi">
+        </form>
+    </div>
+
 </body>
 </html>
